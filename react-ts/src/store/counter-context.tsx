71d3858fcf053744ee690b12af7345e009162b3e
Ehsan-MSI\Ehsan <rsasources@gmail.com>
2023-02-18 12:17:37 +0330

    refactor: build a hook to make it easier to use CounterContext

--- react-ts/src/store/counter-context.tsx
+++ react-ts/src/store/counter-context.tsx
@@ -1,9 +1,9 @@
-import React, { FC, useState } from "react";
+import React, { useState, useContext } from "react";
 import { ReactNode } from "react";
 
 export interface ICounterContext {
   counter: number;
-  setCounter(state: number): void;
+  setCounter(value: any): void;
 }
 
 type MyProps = {
@@ -14,13 +14,26 @@ type MyProps = {
 const CounterContext = React.createContext<ICounterContext | undefined>(
   undefined
 );
+const useCounterContext = (): ICounterContext => {
+  const context = useContext(CounterContext);
 
-export const CounterProvider = (props: MyProps) => {
-  const [counter, setCounter] = useState(15);
+  if (!context) {
+    throw new Error(
+      "useProductsContext must be used within a ProductsProvider"
+    );
+  }
+
+  return context;
+};
+
+const CounterProvider = (props: MyProps) => {
+  const [counter, setCounter] = useState<number>(15);
+
   const CounterContextValue: ICounterContext = {
     counter,
     setCounter,
   };
+
   return (
     <CounterContext.Provider value={CounterContextValue}>
       {props.children}
@@ -28,4 +41,4 @@ export const CounterProvider = (props: MyProps) => {
   );
 };
 
-export default CounterContext;
+export { CounterProvider, useCounterContext };
